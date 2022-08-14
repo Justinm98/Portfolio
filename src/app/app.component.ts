2ffdd2fe19c92a58ba23216a529933a917d6e384
Justin Maloney <jcmaloney1998@gmail.com>
2022-08-14 09:26:37 -0400

    adding styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,40 @@ import {Component} from '@angular/core';
  * AppComponent
  */
 export class AppComponent {
-  title = 'portfolio';
+  DARK_MODE: string = 'dark-mode';
+  LIGHT_MODE: string = 'light-mode';
+  currentTheme: string = this.DARK_MODE;
+
+  /**
+   * Switches app theme to the opposite of current theme
+   */
+  public switchTheme() {
+    const nextTheme: string = this.getOppositeTheme();
+    this.currentTheme = nextTheme;
+    document.body.className = nextTheme;
+  }
+
+  /**
+   * returns the opposite theme string
+   * @return {string} the opposite of the current theme. dark-mode or light-mode
+   */
+  private getOppositeTheme(): string {
+    return this.currentTheme === this.DARK_MODE ? this.LIGHT_MODE : this.DARK_MODE;
+  }
+
+  /**
+   * checks if current theme is dark mode
+   * @return {boolean} true if current theme is dark mode
+   */
+  public isDarkMode(): boolean {
+    return this.currentTheme === this.DARK_MODE;
+  }
+
+  /**
+   * checks if current theme is light mode
+   * @return {boolean} true if current theme is light mode
+   */
+  public isLightMode(): boolean {
+    return this.currentTheme === this.LIGHT_MODE;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
+import {HomepageComponent} from './components/homepage/homepage.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
+  declarations: [AppComponent, HomepageComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatButtonModule, MatIconModule, MatToolbarModule],
   providers: [],
   bootstrap: [AppComponent],
 })
