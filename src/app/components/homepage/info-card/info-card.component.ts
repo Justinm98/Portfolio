777581c181f45bcd4c13e60dea9c691da958461c
Justin Maloney <jcmaloney1998@gmail.com>
2022-08-16 19:01:02 -0400

    added info cards to homepage starting image example implementation for info card

--- src/app/components/homepage/info-card/info-card.component.ts
+++ src/app/components/homepage/info-card/info-card.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-info-card',
@@ -8,9 +8,32 @@ import {Component} from '@angular/core';
 /**
  * InfoCardComponent
  */
-export class InfoCardComponent {
+export class InfoCardComponent implements OnInit {
+  @Input() position: string = 'left';
+  LEFT_STYLE: string = 'info-card-left';
+  RIGHT_STYLE: string = 'info-card-right';
+  public infoCardStyle: string = this.LEFT_STYLE;
+
   /**
    * constructor
    */
   constructor() {}
+
+  /**
+   * ngOnInit
+   */
+  ngOnInit(): void {
+    if (this.isPositionRight()) {
+      console.log(this.position);
+      this.infoCardStyle = this.RIGHT_STYLE;
+    }
+  }
+
+  /**
+   * Check if position is right
+   * @return {boolean} true is position is right
+   */
+  isPositionRight(): boolean {
+    return this.position === 'right';
+  }
 }
