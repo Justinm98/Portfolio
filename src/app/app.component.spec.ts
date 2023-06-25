29ea94740aa384a8b76cc38b6174a8c843e5d147
Justin Maloney <jcmaloney1998@gmail.com>
2023-06-25 09:31:03 -0400

    adding unit tests for toggle button on homepage

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,18 +1,55 @@
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, fakeAsync, tick} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
+import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+
+  it('should default to the dark theme', fakeAsync(() => {
+    expect(component.isCurrentThemeDark()).toBeTrue();
+    expect(component.isCurrentThemeLight()).toBeFalse();
+  }));
+
+  it('should switch the theme to light when you click the theme toggle button once', fakeAsync(() => {
+    spyOn(component, 'switchTheme');
+    const button = fixture.debugElement.query(By.css('.theme-toggle-button')).nativeElement;
+    button.click();
+    tick();
+    expect(component.switchTheme).toHaveBeenCalled();
+  }));
+
+  it('should switch the theme to light when you click the theme toggle button twice', fakeAsync(() => {
+    spyOn(component, 'switchTheme');
+    const button = fixture.debugElement.query(By.css('.theme-toggle-button')).nativeElement;
+    button.click();
+    tick();
+    if (component.isCurrentThemeLight()) {
+      expect(component.isCurrentThemeDark()).toBeFalse();
+      expect(component.isCurrentThemeLight()).toBeTrue();
+    } else {
+      expect(component.isCurrentThemeDark()).toBeTrue();
+      expect(component.isCurrentThemeLight()).toBeFalse();
+    }
+
+    button.click();
+    tick();
+    expect(component.switchTheme).toHaveBeenCalledTimes(2);
+  }));
 });
